fb62b402df8d9dc4b6f6a214a1d40707a7c64cd0
Ywt <1625208758@qq.com>
2026-04-15 10:30:00 +0800

    Initial commit

---
+++ src/main/java/org/example/study/oncallagent/OncallAgentApplication.java
@@ -0,0 +1,13 @@
+package org.example.study.oncallagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncallAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OncallAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/study/oncallagent/OncallAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.study.oncallagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OncallAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
